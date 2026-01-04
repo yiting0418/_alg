67227fedab9586ab9090aa0884f0fbb1c2f2ebd7
yiting0418 <145081180+yiting0418@users.noreply.github.com>
2026-01-05 00:25:18 +0800

    Update hw12.py

--- hw12.py
+++ hw12.py
@@ -24,31 +24,26 @@ def greedy(p_target, q_init, loops=2000, learning_rate=0.5):
     for i in range(loops):
         loss = cross_entropy(p_target, q)
         losses.append(loss)
-        
         gains = p_target / q
         
         idx_best = np.argmax(gains)  #找出最高效益，p大/q小，需要加
         idx_worst = np.argmin(gains) #p小/q大，需要減
         
         diff = gains[idx_best] - gains[idx_worst]
-        
         #收斂判斷
         if diff < 1e-7:
-            print(f"在第 {i} 次迭代時收斂！")
+            print(f"在第 {i} 次迭代時收斂")
             break
         
         #決定加減量
         amount = min(learning_rate * diff * q[idx_worst], q[idx_worst])
-        
         q[idx_best] += amount
         q[idx_worst] -= amount
-
     return q
 
 if __name__ == "__main__":
     #目標p
     p = np.array([1/2, 1/4, 1/4])
-    
     #初始猜測q
     q_start = np.array([1/3, 1/3, 1/3])
     
